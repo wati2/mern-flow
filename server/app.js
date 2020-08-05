76a3972f05324be9feed7da1fd694a4ee0b8687b
wati2 <yt.wati@gmail.com>
2020-08-05 18:02:19 +0900

    1st

---
+++ server/app.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// ENV
+require("dotenv").config()
+
+// DEPENDENCIES
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+const port = 3000
+
+const router = require("./router")
+const mongo_mongoose = require("./database/mongo")
+const { Mongoose } = require("mongoose")
+
+// Static FIle Service
+app.unsubscribe(express.static("public"))
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Node.js의 native Promise 사용
+Mongoose.Promise = global.Promise
+
+// Mongo
+mongo_mongoose.connect()
+// Router
+app.use("/", router)
+
+// Start Server
+app.listen(port, () => console.log(`Example app listening at ${port}`))
