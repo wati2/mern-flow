e3a5fcaf1048029207e7eaf0563ace0f0ccb29b1
jiseong <anrqhwl@gmail.com>
2020-08-12 23:10:18 +0900

    입력창 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import Posts from './components/posts'
+import PostInput from './components/postInput'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div><h4>포스트 쓰기</h4></div>
+        <div>
+          <PostInput/>
+        </div>
+        <hr/>
         <div>
           <Posts/>
         </div>
